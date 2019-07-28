b9bf7ef4268c6d8aad8d1b62f413796cdc0d1bd7
samoylovivan <52442440+samoylovivan@users.noreply.github.com>
2019-07-28 17:45:15 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,15 +3,15 @@ package ivan.samoylov;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(fibonacci(2));
+        System.out.println(numFibonacci(2));
         for(int i = 1; i < 30; i++){
-            System.out.print(fibonacci(i) + " ");
+            System.out.print(numFibonacci(i) + " ");
         }
     }
 
-    public static int fibonacci(int i) {
+    public static int numFibonacci(int i) {
         if (i == 1) return 1;
         if (i == 2) return 1;
-        return fibonacci(i - 1) + fibonacci(i - 2);
+        return numFibonacci(i - 1) + numFibonacci(i - 2);
     }
 }
